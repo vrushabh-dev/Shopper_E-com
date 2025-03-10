d6bf79b7351a2c79c474f1a6903e6ddf0d188afc
vrushabh-dev <vrushabhkunturwar.dev@gmail.com>
2025-03-10 18:06:21 +0530

    Navbar partialy completed

--- frontend/src/Components/Navbar/Navbar.jsx
+++ frontend/src/Components/Navbar/Navbar.jsx
@@ -1,22 +1,29 @@
 import React from "react";
 import "./Navbar.css"
 import logo from '../Assets/logo.png'
-// import cart_icon from '../Assets/cart_icon.png'
+import cart_icon from '../Assets/cart_icon.png'
 
 const Navbar = () => {
     return (
         <>
             <div className="navbar">
-                <div className="logo">
+                <div className="nav-logo">
                     <img src={logo} alt="logo" />
                     <p>SHOPPER</p>
                 </div>
                 <ul className="nav-menu">
-                    <li>Shop</li>
+                    <li>Shop <hr /> </li>
                     <li>Men</li>
                     <li>Women</li>
                     <li>Kids</li>
                 </ul>
+                <div className="nav-login-cart">
+                    <button>Login</button>
+                    <img src={cart_icon} alt="cart_icon" />
+                    <div className="nav-cart-count">
+                        0
+                    </div>
+                </div>
             </div>
         </>
     )
